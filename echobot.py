ae5fb3d1a6570cb32339de4c76b046bfc59da2af
Hansika Liyanage <5010426689@student.csn.edu>
2026-03-07 22:19:45 -0800

    Fix indentation in echobot.py

--- echobot.py
+++ echobot.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 with st.chat_message("user"):
-st.write("Hello ")
+    st.write("Hello")
 
 
 prompt = st.chat_input("Say something")
 if prompt:
-st.write(f"User has sent the following prompt: {prompt}")
+    st.write(f"User has sent the following prompt: {prompt}")
