4d1319b2cca43deb5f68718e4b7b91fcac5531e9
Hansika Liyanage <5010426689@student.csn.edu>
2026-03-07 22:25:20 -0800

    Set up chat history and display previous messages
    Initialize chat history and display messages from session state.

--- echobot.py
+++ echobot.py
@@ -1,9 +1,12 @@
 import streamlit as st
+   
+st.title("Echo Bot")
 
-with st.chat_message("user"):
-    st.write("Hello")
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+       st.markdown(message["content"]
